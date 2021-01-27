8e485a6d85e91dcbd3844ebf6a988ca737fa4c17
Mayank Patel <35776774+Maaayank@users.noreply.github.com>
2021-01-27 18:14:56 +0530

    client cleanup

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,30 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-//import { UserhomeComponent } from './userhome/userhome.component';
+import { AppRoutingModule } from './routing.module/app-routing.module';
+import { AppComponent } from './components/app/app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { UserService } from './user.service';
+import { UserService } from './services/user.service';
 import { HttpClientModule } from '@angular/common/http';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 
 
-import { HomepageComponent } from './homepage/homepage.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
-import { SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login';
-import {
-  GoogleLoginProvider,
-  FacebookLoginProvider
-} from 'angularx-social-login';
-// ,{
-//   provide:AuthServiceConfig
-// }
+import { SocialLoginModule} from 'angularx-social-login';
+
 
 @NgModule({
   declarations: [
@@ -40,7 +33,7 @@ import {
     ReactiveFormsModule,
     HttpClientModule,
     SocialLoginModule,
-    ToastrModule.forRoot(),
+    ToastrModule.forRoot({"positionClass": "toast-bottom-right"}),
     BrowserAnimationsModule,
     SocialLoginModule
   ],
