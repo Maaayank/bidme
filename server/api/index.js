d1ed97b8769063569211af9d935bea62b35b89c6
Mayank Patel <35776774+Maaayank@users.noreply.github.com>
2021-02-17 10:40:13 +0530

    error resolved

--- server/api/index.js
+++ server/api/index.js
@@ -4,6 +4,6 @@ const validateToken = require('../middlewares').validateToken
 router.use('/auth', require('./auth'));
 router.use('/user', validateToken,  require('./user'))
 router.use('/feed', require('./feed'))
-router.use('/product', require('./products'))
+// router.use('/product', require('./products'))
 
 module.exports = router
